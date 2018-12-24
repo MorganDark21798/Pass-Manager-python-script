add1120c4460748947074e50f6755e688d6b3ed4
Morgan Terrance Dark <morganmd1@hotmail.com>
2018-12-24 18:08:13 +0000

    Create Pass.py

---
+++ Pass.py
@@ -0,0 +1,27 @@
+class Login:
+    error = None
+    def __init__(self, uid, passw):
+        self.uid = "198"
+        self.passw = "ADMIN"
+        Login.error = "Enter a valid user id and password"
+
+    def authenticate(self):
+        if (self.uid == logid and self.passw == logpass):
+            print ("Login successful")
+            print ("Pass Manager v1")
+            usrinput = input("Enter the social account: ")
+            if (usrinput == "fb"):
+                    print ("Your Password for Facebook is = *****")
+            if (usrinput == "t"):
+                    print ("Your Password for Twitter is = *****")
+            if (usrinput == "g"):
+                    print ("Your Password for Gmail is = *****")
+    
+        else:
+            print (Login.error)
+log = Login("", "")
+logid = input("Enter your user ID: ")
+logpass = input("Enter your password: ")
+
+
+log.authenticate()
