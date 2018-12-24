6f3be3cd64ba1cbf313e3e4426513b272a1d6a58
Morgan Terrance Dark <morganmd1@hotmail.com>
2018-12-24 19:31:24 +0000

    change able
    Right I am trying to get it so that when it says the password it will ask if it is up to date and if you say no you would be able to change it. but that requires me putting all of the passwords into sepurate variables in order to change them.

--- Pass.py
+++ Pass.py
@@ -4,24 +4,26 @@ class Login:
         self.uid = "198"
         self.passw = "ADMIN"
         Login.error = "Enter a valid user id and password"
+        
 
     def authenticate(self):
         if (self.uid == logid and self.passw == logpass):
             print ("Login successful")
             print ("Pass Manager v1")
-            usrinput = input("Enter the social account: ")
-            if (usrinput == "fb"):
-                    print ("Your Password for Facebook is = *****")
-            if (usrinput == "t"):
-                    print ("Your Password for Twitter is = *****")
-            if (usrinput == "g"):
-                    print ("Your Password for Gmail is = *****")
     
         else:
             print (Login.error)
 log = Login("", "")
 logid = input("Enter your user ID: ")
 logpass = input("Enter your password: ")
-
-
 log.authenticate()
+
+usrinput = input("Enter the social account: ")
+if (usrinput == "fb"):
+    print ("Your Password for Facebook is = *****")
+if (usrinput == "t"):
+    print ("Your Password for Twitter is = *****")
+if (usrinput == "g"):
+    print ("Your Password for Gmail is = *****")
+else:
+    print("error not reconized social account")
